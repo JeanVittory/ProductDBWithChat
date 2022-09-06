54fbab7f15afcc3e1a12c85a320330fbf64b38fa
JeanVittory <jean.carjil@gmail.com>
2022-09-06 01:45:45 -0500

    updating UI data

--- src/test.js
+++ src/test.js
@@ -3,10 +3,5 @@ import { databaseOpt } from './config/knex.config.js';
 import { ChatServices } from './services/chat.services.js';
 import { ProductsServices } from './services/products.services.js';
 
-// ir borrando
-import { Contenedor } from './class/container.class.js';
-export const products = new Contenedor('./src/products.txt');
-
-//new database
 export const chatServices = new ChatServices(sqliteOpt, 'chatMessages');
 export const productsServices = new ProductsServices(databaseOpt, 'products');
